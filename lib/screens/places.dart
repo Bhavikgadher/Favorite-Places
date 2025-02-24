c7236a01552dbc321a0be4124c004f5ec49849d3
Bhavikgadher <gadherbhavik@gmail.com>
2025-02-25 01:30:16 +0530

    Use RiverPod for State Management

---
+++ lib/screens/places.dart
@@ -0,0 +1,36 @@
+import 'package:favorite_places/providers/user_places.dart';
+import 'package:favorite_places/widgets/places_list.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'add_place.dart';
+
+class PlacesScreen extends ConsumerWidget {
+  const PlacesScreen({super.key});
+
+  void _addPlace() {}
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlaceProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ct) => const AddPlaceScreen(),
+                ),
+              );
+            },
+            icon: const Icon(CupertinoIcons.add),
+          )
+        ],
+      ),
+      body: PlacesList(places: userPlaces),
+    );
+  }
+}
